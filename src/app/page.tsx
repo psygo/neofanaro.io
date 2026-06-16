1332106df9546ddf2b724f7352b2dc4a6fd7c18b
Philippe Fanaro <philippefanaro@gmail.com>
2026-06-16 08:14:19 -0300

    rearranging the file structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Nav from "./components/nav";
-import Footer from "./components/footer";
+import Nav from "../components/nav";
+import Footer from "../components/footer";
 
 export default function Home() {
   return (
