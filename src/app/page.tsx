67e521089156d272c8ceba0a8047873e3c0a2f0b
Philippe Fanaro <philippefanaro@gmail.com>
2026-06-18 09:33:53 -0300

    a container for FIC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,11 @@
 import Image from "next/image"
 
 import useLang from "@hooks/useLang"
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center gap-4">
+    <main className="flex flex-col items-center gap-12">
       <PresentationSection />
       <SoftwareWorkSection />
     </main>
@@ -16,11 +17,16 @@ export default function Home() {
 // ---------------------------------------------------------
 // Presentation Section
 
+const containerOutline =
+  "rounded-xl shadow-lg ring-1 ring-slate-200"
+
 function PresentationSection() {
   const lang = useLang()
 
   return (
-    <section className="flex max-w-xl flex-col items-center gap-6 rounded-xl bg-slate-50 px-4 py-6 shadow-lg ring-1 ring-slate-200 sm:flex-row sm:gap-8 sm:px-7">
+    <section
+      className={`${containerOutline} flex max-w-80 flex-col items-center gap-5.5 px-4 py-6 sm:max-w-xl sm:flex-row sm:gap-8 sm:px-7`}
+    >
       <div className="flex flex-col gap-4">
         <h2 className="text-center text-3xl font-bold sm:text-left sm:text-4xl">
           Philippe Fanaro
@@ -41,6 +47,7 @@ function PresentationSection() {
         )}
       </div>
       <Image
+        loading="eager"
         src="/pictures/philippe_playing_go.png"
         alt="Fanaro"
         width={200}
@@ -68,15 +75,51 @@ function PresentationParagraph({
 
 function SoftwareWorkSection() {
   return (
-    <section>
-      {/* <iframe
-        id="inlineFrameExample"
-        title="Inline Frame Example"
-        width="300"
-        height="200"
-        src="https://aquarifolio.vercel.app"
-      ></iframe> */}
+    <section
+      className={`${containerOutline} flex max-w-80 flex-col gap-6 px-6 py-6 sm:max-w-xl`}
+    >
+      <h2 className="text-2xl font-bold">
+        Get to know some of my software work:
+      </h2>
+      <div className="flex gap-4">
+        <Fic />
+      </div>
     </section>
+  )
+}
+
+function Fic() {
+  return (
+    <Link
+      href="https://pub.dev/packages/fast_immutable_collections"
+      className={`${containerOutline} flex w-50 flex-col items-center gap-2 bg-gray-50 px-2 py-4`}
+    >
+      <Image
+        loading="eager"
+        src="/software_work/fic.svg"
+        alt="FIC"
+        width={80}
+        height={80}
+      />
+      <div className="flex flex-col items-center gap-1">
+        <h3 className="text-center text-lg font-bold">
+          Fast Immutable Collections
+        </h3>
+        <p className="text-center">
+          Immutable Collections for Dart/Flutter
+        </p>
+        <div className="mt-1 flex gap-1">
+          <Image
+            loading="eager"
+            src="/software_work/star.svg"
+            alt="FIC"
+            width={21.5}
+            height={21.5}
+          />
+          <p className="font-medium">200+</p>
+        </div>
+      </div>
+    </Link>
   )
 }
 
