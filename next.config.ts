96e95db89064953c78e9cfabc7285621698d165d
Philippe Fanaro <philippefanaro@gmail.com>
2026-06-18 13:38:35 -0300

    trying to fix Next.js problems with exports 2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  experimental: {
+    swcPlugins: [], // If you are using specific swc decorator plugins
+  },
+}
 
-export default nextConfig;
+export default nextConfig
