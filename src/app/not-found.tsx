9e581f1b203ef7d1e8d2d65b44b4921abaeddb82
Philippe Fanaro <philippefanaro@gmail.com>
2026-06-21 09:01:53 -0300

    lang Not Found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,36 @@
-// import { Main } from "@components/main"
+"use client"
 
 import { Suspense } from "react"
 
+import { useLang } from "@hooks/useLang"
+
+import { Main } from "@components/main"
+
 export default function NotFound() {
   return (
-    <div>
-      <Suspense>
-        <p>hello</p>
-      </Suspense>
-    </div>
+    <Suspense>
+      <NotFoundMessage />
+    </Suspense>
+  )
+}
+
+function NotFoundMessage() {
+  const lang = useLang()
+
+  return (
+    <Main>
+      <div className="mx-auto mt-10 mb-6 flex flex-col gap-4 text-center">
+        <h2 className="text-3xl font-bold">
+          {lang === "pt"
+            ? "Página Não Encontrada"
+            : "Page Not Found"}
+        </h2>
+        <p>
+          {lang === "pt"
+            ? "Não foi possível encontrar o conteúdo que você estava procurando."
+            : "Could not find the requested resource."}
+        </p>
+      </div>
+    </Main>
   )
 }
