14d590174c9b55a1bd68b437f9bd8fd591218e52
Philippe Fanaro <philippefanaro@gmail.com>
2026-06-28 16:40:12 -0300

    trying to fix the social media preview logo

--- src/server/utils/generateMetadataHelper.ts
+++ src/server/utils/generateMetadataHelper.ts
@@ -45,7 +45,7 @@ export async function generatePostMetadataHelper(
           description: post.description,
           images: [
             {
-              url: "https://neofanaroio.vercel.app/logos/favicon.svg",
+              url: "https://neofanaroio.vercel.app/logos/favicon.io.svg",
             },
           ],
         },
