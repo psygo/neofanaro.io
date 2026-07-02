789bc4c02f3232d42d8cdb0235835d367203763e
Philippe Fanaro <philippefanaro@gmail.com>
2026-07-02 16:25:08 -0300

    the black border on the post cards is too much

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -20,6 +20,6 @@ export const containerOutline =
   "rounded-xl border border-gray-300 px-4 pt-3 pb-3.5 transition-all duration-300 ease-in-out hover:bg-gray-100 hover:duration-200"
 
 export const cardDecoration =
-  "rounded-xl border-2 border-gray-700 px-4 pt-3 pb-3.5 shadow-md transition-all duration-300 ease-in-out hover:-translate-y-0.5 hover:bg-gray-100 hover:shadow-xl hover:duration-200"
+  "rounded-xl border-2 border-gray-300 px-4 pt-3 pb-3.5 shadow-md transition-all duration-300 ease-in-out hover:-translate-y-0.5 hover:bg-gray-100 hover:shadow-xl hover:duration-200"
 
 // ---------------------------------------------------------
