810118e9d72dcd4f25f447fc50acd35565355fa9
Philippe Fanaro <philippefanaro@gmail.com>
2026-07-30 11:21:54 -0300

    editing the Lang101 article

--- src/components/posts/articles/lang101.tsx
+++ src/components/posts/articles/lang101.tsx
@@ -18,41 +18,54 @@ export function Lang101({ post }: BlogPostProps) {
           I&apos;m happy to announce the release of my first
           mobile app in a long while, an idea I had been
           brewing for a long time, out of my frustrations
-          with language learning apps and ecosystem .
+          with language learning apps and overall ecosystem.
         </PostParagraph>
+        <PostImageWithLegend
+          src="/articles/lang101/Screenshot Alpha 1.0 - 3.png"
+          height={300}
+          width={250}
+          className="rounded-lg"
+        >
+          <p>The &quot;Exercises&quot; page on Lang101.</p>
+        </PostImageWithLegend>
         <PostBlockQuote>
           Lang101 also features a{" "}
           <PostLink href="https://lang101.expo.app/">
-            web interface.
+            web interface
           </PostLink>
+          .
         </PostBlockQuote>
         <PostParagraph>
           <PostLink href="https://expo.dev/accounts/psygo/projects/lang101/builds/7e036e2f-8647-42b7-8062-0313b29aba27">
             Lang101
           </PostLink>{" "}
-          is the fusion of{" "}
-          <PostLink href="https://en.wikipedia.org/wiki/Anki">
-            Anki
-          </PostLink>
-          , a statistical rating system &mdash; in this
-          case,{" "}
-          <PostLink href="https://en.wikipedia.org/wiki/Elo_rating_system">
-            Elo
-          </PostLink>{" "}
-          &mdash; and social features, all in the context of
-          language learning. I&apos;ve also taken quite a
-          bit of inspiration from{" "}
+          is the fusion of :
+        </PostParagraph>
+        <PostUnorderedList>
+          <li>
+            <PostLink href="https://en.wikipedia.org/wiki/Anki">
+              Anki
+            </PostLink>
+          </li>
+          <li>
+            A Statistical Rating Sytem &mdash; more
+            specifically,{" "}
+            <PostLink href="https://en.wikipedia.org/wiki/Elo_rating_system">
+              Elo
+            </PostLink>
+          </li>
+          <li>Social Features</li>
+        </PostUnorderedList>
+
+        <PostParagraph>
+          I&apos;ve also taken quite a bit of inspiration
+          from{" "}
           <PostLink href="https://101weiqi.com">
             101weiqi
           </PostLink>
           , a popular website for practicing Go (board game)
           problems.
         </PostParagraph>
-        <PostUnorderedList>
-          <li>Anki</li>
-          <li>A Statistical Rating Sytem</li>
-          <li>Social features</li>
-        </PostUnorderedList>
         <PostImageWithLegend
           src="/articles/lang101/Screenshot Alpha 1.0 - 1.png"
           height={300}
@@ -65,16 +78,37 @@ export function Lang101({ post }: BlogPostProps) {
           </p>
         </PostImageWithLegend>
         <PostParagraph>
-          The Elo system is a way for us to not depend on
-          human rating anymore, which is a very common
-          problem in language learning. And that problem is
-          manifold, as it is a burden on the instructors
-          while also creating a less engaging experience for
-          the learners. A more accurate difficulty rating
-          system also reflects the real world much better,
-          so the student&apos;s learning experience can only
-          improve.
+          A statistical rating system is self-adjusting,
+          while in practice mode, both the student&apos;s
+          and the exercise&apos;s ratings get readjusted
+          proportionally to relative difficulty. If the
+          exercise is supposedly too easy for the student,
+          if the student fails it, the rating penalty is big
+          while the exercise also gets a large boost to its
+          rating. On the other hand, if the student solves
+          it correctly, only a few points will be earned.
         </PostParagraph>
+        <PostParagraph>
+          By avoiding a dependence on instructors to level
+          problem difficulty, not only do we relieve
+          ourselves from a clunky burden, but create a more
+          engaging experience, as we get challenges which
+          reflect the real world much more closely.
+        </PostParagraph>
+        <PostImageWithLegend
+          src="/articles/lang101/Screenshot Alpha 1.0 - 9.png"
+          height={300}
+          width={250}
+          className="rounded-lg"
+        >
+          <p>
+            The student loses 19 points for solving a
+            problem incorrectly. The 19 points are based on
+            his Elo rating prior to the question, if that
+            rating had been higher, an even bigger penalty
+            would be applied.
+          </p>
+        </PostImageWithLegend>
         <PostParagraph>
           Anki does feature a rating system, typically based
           on the user&apos;s difficulty in recalling an
@@ -82,40 +116,24 @@ export function Lang101({ post }: BlogPostProps) {
           terms of granularity, very subjective, and quite
           tiring, as not only will you have to practice the
           cards but also worry about how to classify your
-          answers.
+          results.
         </PostParagraph>
         <PostParagraph>
-          Lang101&apos;s rating system is based on numbers,
-          starting from around 1000, going up or down
-          according to the user&apos;s answers. If the user
-          happens to solve a difficult exercise with respect
-          to his or her rating, the points reward will be
-          bigger than if the exercise were easier. In that
-          way, the whole system is self-adjusting, across
-          all users and exercises.
-        </PostParagraph>
-        <PostParagraph>
-          Besides the key feature of a rating syste, Lang101
-          also offers:
+          Besides the key feature of a rating system,
+          Lang101 also offers:
         </PostParagraph>
         <PostUnorderedList>
           <li>Per-language question filtering</li>
+          <li>Per-language question prompt filtering</li>
           <li>Leaderboards</li>
           <li>History of solved exercises</li>
           <li>Comments on exercises</li>
           <li>Ratings on exercises</li>
           <li>Rating graphs</li>
           <li>Exercise list creation</li>
-          <li>User following and messaging</li>
+          <li>User following</li>
+          <li>User messaging</li>
         </PostUnorderedList>
-        <PostImageWithLegend
-          src="/articles/lang101/Screenshot Alpha 1.0 - 2.png"
-          height={300}
-          width={250}
-          className="rounded-lg"
-        >
-          <p>The &quot;Exercises&quot; page on the app.</p>
-        </PostImageWithLegend>
         <PostImageWithLegend
           src="/articles/lang101/Screenshot Alpha 1.0 - 4.png"
           height={300}
@@ -123,10 +141,31 @@ export function Lang101({ post }: BlogPostProps) {
           className="rounded-lg"
         >
           <p>
-            The &quot;History&quot; page on Lang101, on dark
+            Lang101&apos;s &quot;History&quot; page, on dark
             mode.
           </p>
         </PostImageWithLegend>
+        <PostParagraph>
+          The current version of the app also offer exercise
+          variety, which will only increase over time:
+        </PostParagraph>
+        <PostUnorderedList>
+          <li>Multiple choice</li>
+          <li>Type the answer</li>
+          <li>Listening to a (YouTube) video</li>
+        </PostUnorderedList>
+        <PostParagraph>
+          I have plenty more features in line for the near
+          future, such as a multiplayer mode and better
+          integration with Anki decks.
+        </PostParagraph>
+        <PostParagraph>
+          At the moment, as a first seed, a lot of the
+          questions have been either created by AI, or
+          batch-transcribed by it, based on open-source Anki
+          decks. But, as the community gets bigger, the
+          human-created percentage will hopefully increase.
+        </PostParagraph>
       </PostSection>
     </Post>
   )
